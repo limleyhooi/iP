c5fd4555e197445b8638ee4e3b4cec6d754acdcf
limleyhooi <leyhooi88@gmail.com>
2025-02-08 22:29:47 +0800

    level 1

--- src/main/java/Rick.java
+++ src/main/java/Rick.java
@@ -1,16 +1,33 @@
+import java.util.Scanner ;
+
 public class Rick {
     public static void main(String[] args) {
         String logo =
                 " ____  _      _    \n" +
-                "|  _ \\(_) ___| | __\n" +
-                "| |_) | |/ __| |/ /\n" +
-                "|  _ <| | (__|   < \n" +
-                "|_| \\_\\_|\\___|_|\\_\\";
+                        "|  _ \\(_) ___| | __\n" +
+                        "| |_) | |/ __| |/ /\n" +
+                        "|  _ <| | (__|   < \n" +
+                        "|_| \\_\\_|\\___|_|\\_\\";
         System.out.println("Hello from\n" + logo);
         System.out.println("""
                 Hello, I'm rick
                 What can I do for you?
-                Bye. Hope to see you again soon!""");
+                """);
+        Scanner s = new Scanner(System.in);
 
+        while (true) {
+            System.out.print(">");
+            String input = s.nextLine();
+
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("bye bye! Hope to see you soon ^-^ ");
+                break;
+            }
+
+
+            System.out.println(input);
+        }
     }
 }
+
