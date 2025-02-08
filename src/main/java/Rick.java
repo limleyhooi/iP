817846f44654d995add07306ddc12d3dddeaefae
limleyhooi <leyhooi88@gmail.com>
2025-02-08 19:55:37 +0800

    level 0

---
+++ src/main/java/Rick.java
@@ -0,0 +1,16 @@
+public class Rick {
+    public static void main(String[] args) {
+        String logo =
+                " ____  _      _    \n" +
+                "|  _ \\(_) ___| | __\n" +
+                "| |_) | |/ __| |/ /\n" +
+                "|  _ <| | (__|   < \n" +
+                "|_| \\_\\_|\\___|_|\\_\\";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                Hello, I'm rick
+                What can I do for you?
+                Bye. Hope to see you again soon!""");
+
+    }
+}
