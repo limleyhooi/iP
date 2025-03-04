073a2020df4d920570052075736f70b96969b70c
limleyhooi <leyhooi88@gmail.com>
2025-03-05 07:58:41 +0800

    added delete command

--- src/main/java/Rick.java
+++ src/main/java/Rick.java
@@ -1,4 +1,5 @@
 import java.util.Scanner ;
+import java.util.ArrayList;
 
 public class Rick {
     private static final String DIVIDER = "    ____________________________________________________________";
@@ -20,13 +21,14 @@ public class Rick {
                 What can I do for you?
                 """);
         Scanner s = new Scanner(System.in);
-        Task[] tasks = new Task[100]; //create an Task array called tasks that hold 100 Task elements;
+        ArrayList<Task> tasks = new ArrayList<>();
         int i = 0;
+
 
         while (true) {
             System.out.print(">");
-            String input = s.nextLine(); //enter string element;
-            input = input.replaceAll("\\s","");//remove all spaces in user input.
+            String originalInput = s.nextLine(); //enter string element;
+            String input = originalInput.replaceAll("\\s","");//remove all spaces in user input.
 
 
             if (input.startsWith("bye")) {
@@ -34,13 +36,15 @@ public class Rick {
                 break;
             }
             if (input.startsWith("list")){
-                if (i ==0) {
+                if (tasks.isEmpty()) {
                     System.out.println(" No tasks added (;ↀ⌓ↀ)"); //tasks array is empty
                 } else {
                     System.out.println("Here are the items in your list ᕦ(ò_óˇ)ᕤ :");
-                    for (int j = 0; j < i; j++) {
-                        System.out.print(j + 1+"."); //iterate through tasks array;
-                        System.out.println(tasks[j]);
+                    int arrayIndex = 0;
+                    for (Task task :tasks) {
+                        System.out.print(arrayIndex + 1+"."); //iterate through tasks array;
+                        System.out.println(task.toString());
+                        arrayIndex++;
 
                     }
                 }
@@ -51,10 +55,10 @@ public class Rick {
                 try {
                     taskIndex = Integer.parseInt(input.substring(4)) - 1; //finding the index to mark;
                     if (taskIndex >= 0 && taskIndex < i) {
-                        tasks[taskIndex].markAsDone();
+                        tasks.get(taskIndex).markAsDone();
                         printDivider();
                         System.out.println(" Nicee! I've marked this task as done ᕕ( ᐛ )ᕗ :");
-                        System.out.println(tasks[taskIndex]);
+                        System.out.println(tasks.get(taskIndex));
                         printDivider();
                     } else {
                         System.out.println("Invalid task number!");
@@ -69,10 +73,10 @@ public class Rick {
                 try {
                     taskIndex = Integer.parseInt(input.substring(6)) - 1;
                     if (taskIndex >= 0 && taskIndex < i) {
-                        tasks[taskIndex].markAsUndone();
+                        tasks.get(taskIndex).markAsUndone();
                         printDivider();
                         System.out.println(" Gotcha! I've unmarked this task (≧▽≦)");
-                        System.out.println(tasks[taskIndex]);
+                        System.out.println(tasks.get(taskIndex));
                         printDivider();
                     } else {
                         System.out.println("Invalid task number!");
@@ -91,31 +95,43 @@ public class Rick {
                     System.out.println("Your todo task needs a description with keyword: todo"+ System.lineSeparator()+"E.g. todo borrow books");
                     continue;
                 }
-                tasks[i] = new Todo(input);
+                tasks.add(new Todo(input));
             }
             if(input.startsWith("deadline")){
                 String modifiedInput = input.substring(8);//remove the word deadline;
                 try{
                 if(modifiedInput.isEmpty()){
                     throw new RickException();
-                }}catch(RickException e){
-                System.out.println("Your deadline task needs a description with keywords: deadline, /by" + System.lineSeparator()+" E.g. deadline return book /by sunday ");
-                continue;
-            }
+                }
+                if (!modifiedInput.contains("/by")) {//check if user input contains keyword "/by"
+                    throw new RickException();
+                }}catch(RickException e) {
+                    System.out.println("Your deadline task needs a description with keywords: deadline, /by" + System.lineSeparator() + " E.g. deadline return book /by sunday ");
+                    continue;
+                } catch(ArrayIndexOutOfBoundsException e){
+                    System.out.println("Please provide both a description and a date using the /by keyword.");
+                }
                 String[] part = modifiedInput.split("/by",2); //split modified string to description and date;
                 String Deadline_description = part[0].trim();
                 String Deadline_date = part[1].trim();
-                tasks[i] = new Deadline(Deadline_description,Deadline_date);
+                tasks.add(new Deadline(Deadline_description,Deadline_date));
 
 
             }
-            if(input.startsWith("event")){
+            if(input.startsWith("event")) {
                 String modifiedInput = input.substring(5);
-                try{
-                    if(modifiedInput.isEmpty()){
+                try {
+                    if (modifiedInput.isEmpty()) {
                         throw new RickException();
-                    }}catch(RickException e) {
-                    System.out.println(" Your event task needs a description with keywords: event, /from & /to" +System.lineSeparator()+"E.g. event project meeting /from Mon 2pm /to 4pm ");
+                    }
+                    if (!modifiedInput.contains("/from") | !modifiedInput.contains("/to")) {
+                        throw new RickException();
+                    }
+                } catch (RickException e) {
+                    System.out.println(" Your event task needs a description with keywords: event, /from & /to" + System.lineSeparator() + "E.g. event project meeting /from Mon 2pm /to 4pm ");
+                    continue;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Please provide both a description and a date using the /from & /to keyword.");
                     continue;
                 }
                 String[] part = modifiedInput.split("/from|/to", 3);
@@ -123,17 +139,43 @@ public class Rick {
                 String time1 = part[1].trim();
                 String time2 = part[2].trim();
                 int eventIndex = i + 1;
-                tasks[i] = new Event(description, time1, time2);
-
+                tasks.add(new Event(description, time1, time2));
 
             }
-            if(tasks[i] ==null){
-                System.out.println("sorry pal, not a recognized command ಸ‿ಸ ");
+            if(input.startsWith("delete")) {
+                String modified = input.substring(6).trim();
+                try {
+                    if (modified.isEmpty()) {
+                        throw new RickException();
+                    }
+                    int deleteIndex = Integer.parseInt(modified) - 1;
+                    tasks.remove(deleteIndex);
+                } catch (RickException e) {
+                    System.out.println("what do I help you delete?");
+                    continue;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid Number!");
+                    continue;
+                } catch (NumberFormatException e) {
+                    System.out.print("Enter a number!");
+                    continue;
+                }
+                System.out.println("okie deleted!");
+                continue;
+            }
+
+
+
+            try {
+                Task task = tasks.get(i);
+
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("sorry pal, not a recognized command");
                 continue;
             }
 
             System.out.println("Okie doki, added to task! ʕ•ᴥ•ʔ ");
-            System.out.println(tasks[i]);
+            System.out.println(tasks.get(i).toString());
             System.out.println("Now you have "+ String.valueOf(i + 1)+ " tasks in list ᕙ(⇀‸↼‶)ᕗ");
             i++;
 
